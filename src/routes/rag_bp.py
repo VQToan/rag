34dc26af34f2f628d830936e5583e0f27a25e0d1
toan.vq <toan.vq@roviholdings.vn>
2025-01-14 09:59:59 +0700

    add ui demo

--- src/routes/rag_bp.py
+++ src/routes/rag_bp.py
@@ -4,13 +4,17 @@ from src.controllers import add_knowledge, remove_knowledge, get_knowledge, get_
 
 rag_bp = Blueprint('rag_bp', __name__)
 
+
 def welcome():
     return 'Welcome to RAG API'
+
 
 def demo():
     return render_template('ui_test.html')
 
+
 rag_bp.route('/')(welcome)
+rag_bp.route('/liveness')(welcome)
 rag_bp.route('/get', methods=['POST'])(get_knowledge)
 rag_bp.route('/add', methods=['POST'])(add_knowledge)
 rag_bp.route('/add_all', methods=['POST'])(add_knowledge_all)
